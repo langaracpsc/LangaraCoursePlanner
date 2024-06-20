09776879192476ac0ac4b3940dfe907b757e09f8
Anderson T <tseng.andersonn@gmail.com>
2024-06-20 05:25:46 -0700

    add subject and all_courses route

--- main.py
+++ main.py
@@ -40,6 +40,75 @@ def format_attribute(value):
         return "no"
     return value
 
+@app.route('/course')
+def all_courses():
+    api_url = f"https://coursesapi.langaracs.ca/index/courses"
+    response = requests.get(api_url)
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to fetch data'}), response.status_code
+    
+    data = response.json()
+    
+    subjects = {}
+    active_c = 0
+    active_s = 0
+    
+    for course in data['courses']:
+        if course['subject'] not in subjects:
+            subjects[course['subject']] = ([], [])
+       
+        if course["active"]:
+            subjects[course['subject']][0].append(course)
+            active_c+=1
+        else:
+            subjects[course['subject']][1].append(course)    
+    
+    for s in subjects:
+        if len(subjects[s][0]) > 0:
+            active_s+=1
+
+    return render_template('all_courses.html', 
+                           count_s=len(subjects), 
+                           count_s_active=active_s,
+                           count_s_inactive=len(subjects)-active_s,
+                           count_c=len(data['courses']), 
+                           count_c_active=active_c,
+                           count_c_inactive=len(data['courses'])-active_c,
+                           subject="All Courses", 
+                           subjects=subjects)
+        
+
+# route for getting all courses for a subject
+@app.route('/course/<department>', strict_slashes=False)
+def subject(department:str):
+    api_url = f"https://coursesapi.langaracs.ca/index/courses"
+    response = requests.get(api_url)
+    if response.status_code != 200:
+        return jsonify({'error': 'Failed to fetch data'}), response.status_code
+    
+    data = response.json()
+        
+    subject_courses:list[dict] = []
+    
+    department=department.upper()
+    for c in data["courses"]:
+        if c['subject'] == department:
+            subject_courses.append(c)
+    
+    if len(subject_courses) == 0:
+        return jsonify({'error': 'Nothing found for that subject.'}), 404
+    
+    active_courses = []
+    inactive_courses = []
+    
+    for c in subject_courses:
+        if c["active"]:
+            active_courses.append(c)
+        else:
+            inactive_courses.append(c)
+    
+    return render_template('subject.html', subject=department, active_courses=active_courses, inactive_courses=inactive_courses)
+        
 
 # route for getting specific course
 @app.route('/course/<department>/<course_number>')
